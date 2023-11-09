2b0e7398821ddef9beb64b81a2cf50ce0b47221c
GitWils <yryabchenko@gmail.com>
2023-11-09 16:47:45 +0200

    added some widgets and stylised it

--- main.py
+++ main.py
@@ -14,16 +14,74 @@ class Project(QtWidgets.QWidget):
         #self.setGeometry(400, 400, 950, 690)
         self.center()
         self.initColors()
-        self.setWindowTitle('CMan')
+        self.initMenu()
+        self.setWindowTitle('Облік договорів, комплектуючих')
         self.show()
+
+    def initMenu(self):
+        vMenu = QtWidgets.QVBoxLayout()
+        self.vbox = QtWidgets.QVBoxLayout()
+
+        lblLog = QtWidgets.QLabel("<b>Журнал подій:</b>")
+        self.logArea = QtWidgets.QTextEdit('№1: <span style="color: #153">31.10.2023</span> створено договір ' +
+                                      '<span style="text-decoration: underline">№171</span>', parent=self)
+        self.logArea.setReadOnly(True)
+
+        self.vbox.setStretch(0, 15)
+        self.vbox.setStretch(1, 1)
+        self.vbox.setStretch(2, 3)
+
+        self.initVMenu()
+        self.initVBox()
+        self.vbox.addWidget(lblLog)
+        self.vbox.addWidget(self.logArea, QtCore.Qt.AlignmentFlag.AlignBottom)
+
+        self.mainArea = QtWidgets.QHBoxLayout()
+        self.mainArea.addLayout(self.vMenu)
+        self.mainArea.addLayout(self.vbox)
+        self.mainArea.setSpacing(20)
+        self.setLayout(self.mainArea)
+
+
+    def initVBox(self):
+        pass
+        #vbox = QtWidgets.QVBoxLayout()
+        #vbox.addWidget(table)
+        #vbox.addWidget(lblLog)
+        #vbox.addWidget(logArea, QtCore.Qt.AlignmentFlag.AlignBottom)
+        #vbox.setStretch(0, 15)
+        #vbox.setStretch(1, 1)
+        #vbox.setStretch(2, 3)
+
+    def initVMenu(self):
+        #buttons
+        button1 = QtWidgets.QPushButton("Створити договір")
+        #button1.setStyleSheet("background-color: {}; ".format(self.btnClr.name()))
+        button2 = QtWidgets.QPushButton("Редагувати договір")
+        button3 = QtWidgets.QPushButton("3")
+        button3.setDisabled(True)
+        button4 = QtWidgets.QPushButton("4")
+        button4.setDisabled(True)
+        button5 = QtWidgets.QPushButton("Фільтр")
+
+        self.vMenu = QtWidgets.QVBoxLayout()
+        self.vMenu.addWidget(button1)
+        self.vMenu.addWidget(button2)
+        self.vMenu.addWidget(button3)
+        self.vMenu.addWidget(button4)
+        self.vMenu.addWidget(button5)
+        self.vMenu.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+        self.vMenu.addStretch(40)
+        #self.setLayout(self.vMenu)
 
     def initColors(self):
         #using colors: #0d1321, #1d2d44, #3e5c76, #748cab, #f0ebd8
-        bgnd = QtGui.QColor("#1d2d44")
-        inctbgnd = QtGui.QColor("#0d1321")
+        self.bgndClr = QtGui.QColor('#1d2d44')
+        self.inctBgndClr = QtGui.QColor('#0d1321')
+        self.btnClr = QtGui.QColor('#3e5c76')
         pal = self.palette()
-        pal.setColor(QtGui.QPalette.ColorGroup.Normal, QtGui.QPalette.ColorRole.Window, bgnd)
-        pal.setColor(QtGui.QPalette.ColorGroup.Inactive, QtGui.QPalette.ColorRole.Window, inctbgnd)
+        pal.setColor(QtGui.QPalette.ColorGroup.Normal, QtGui.QPalette.ColorRole.Window, self.bgndClr)
+        pal.setColor(QtGui.QPalette.ColorGroup.Inactive, QtGui.QPalette.ColorRole.Window, self.inctBgndClr)
         self.setPalette(pal)
 
     def center(self):
@@ -36,6 +94,31 @@ def main():
     app = QtWidgets.QApplication(sys.argv)
     ico = QtGui.QIcon("img/logo.png")
     app.setWindowIcon(ico)
+    app.setStyleSheet("""
+        QWidget {
+            background-color: "#1d2d44";
+            color: "#f0ebd8";
+        }
+        QPushButton {
+            font-size: 16px;
+            color: "#f0ebd8";
+            background-color: "#3e5c76"
+        }
+        QPushButton:pressed {
+            font-size: 16px;
+            color: "#f0ebd8";
+            background-color: "#1d2d44"
+        }
+        QPushButton:disabled {
+            font-size: 16px;
+            color: "#f0ebd8";
+            background-color: "#1d2d44"
+        }
+        QTextEdit, QLineEdit {
+            background-color: "#748cab";       
+            color: "#1d2d44"     
+        }
+    """)
     pr = Project()
     sys.exit(app.exec())
 
