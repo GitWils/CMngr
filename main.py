71d095798bed7e43cf62f0cb55948e6ee056bdf2
GitWils <yryabchenko@gmail.com>
2023-11-09 09:47:56 +0200

    creating simple window app

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-def main():
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi,')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt6 import QtGui, QtWidgets, QtCore
+import sys
 
+class Project(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.resize(950, 690)
+        self.center()
+        self.setWindowTitle('CMan')
+        self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = self.screen().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topRight())
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    pr = Project()
+    sys.exit(app.exec())
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
