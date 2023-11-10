0a90afb3dc1e631522a2f4594ab2a0ae9b8d67b4
GitWils <yryabchenko@gmail.com>
2023-11-10 16:30:23 +0200

    split main file and stylesheet file

--- main.py
+++ main.py
@@ -20,43 +20,80 @@ class Project(QtWidgets.QWidget):
 
     def initMenu(self):
         vMenu = QtWidgets.QVBoxLayout()
-        self.vbox = QtWidgets.QVBoxLayout()
-
-        lblLog = QtWidgets.QLabel("<b>Журнал подій:</b>")
-        self.logArea = QtWidgets.QTextEdit('№1: <span style="color: #153">31.10.2023</span> створено договір ' +
-                                      '<span style="text-decoration: underline">№171</span>', parent=self)
-        self.logArea.setReadOnly(True)
-
-        self.vbox.setStretch(0, 15)
-        self.vbox.setStretch(1, 1)
-        self.vbox.setStretch(2, 3)
-
         self.initVMenu()
-        self.initVBox()
-        self.vbox.addWidget(lblLog)
-        self.vbox.addWidget(self.logArea, QtCore.Qt.AlignmentFlag.AlignBottom)
+        #self.mainArea.setSpacing(20)
+        self.__initLayout1()
+        self.__initLayout0()
 
-        self.mainArea = QtWidgets.QHBoxLayout()
-        self.mainArea.addLayout(self.vMenu)
-        self.mainArea.addLayout(self.vbox)
-        self.mainArea.setSpacing(20)
-        self.setLayout(self.mainArea)
+    def __initLayout0(self):
+        lblLog = QtWidgets.QLabel("<b>Журнал подій:</b>")
+        logArea = QtWidgets.QTextEdit('№1: <span style="color: #153">31.10.2023</span> створено договір ' +
+                                           '<span style="text-decoration: underline">№171</span>', parent = self)
+        logArea.setReadOnly(True)
+        self.vbox = QtWidgets.QVBoxLayout()
+        self.vbox.addLayout(self.innerbox)
+        logs = QtWidgets.QVBoxLayout()
+        logs.addWidget(lblLog)
+        logs.addWidget(logArea, QtCore.Qt.AlignmentFlag.AlignBottom)
+        self.vbox.addLayout(logs)
+        self.vbox.setStretch(0, 3)
+        self.vbox.setStretch(1, 1)
+        self.setLayout(self.vbox)
+
+    def __initLayout1(self):
+        self.innerbox = QtWidgets.QHBoxLayout()
+        self.innerbox.addLayout(self.vMenu)
+        mainArea = QtWidgets.QVBoxLayout()
+
+        # Table
+        table = QtWidgets.QTableView(parent=self)
+        table.setProperty("class", "items")
+        sti = QtGui.QStandardItemModel(parent=self)
+        lst1 = ['Шайба', 'Втулка', 'Корпус', 'Тара', 'Перехідник']
+        lst2 = ['30', '100', '50', '120', '220']  #
+        lst3 = ['100', '100', '300', '150', '200']  # needed
+        lst4 = ['171', '171', '171', '171', '171']  # contract
+        for row in range(0, 5):
+            # if row == 2:
+            #    iconfile = 'logo.png'
+            # else:
+            #    iconfile = 'logo.png'
+            item1 = QtGui.QStandardItem(lst1[row])
+            item2 = QtGui.QStandardItem(lst2[row])
+            item3 = QtGui.QStandardItem(lst3[row])
+            val = int(lst2[row]) - int(lst3[row])
+            item4 = QtGui.QStandardItem(str(val))
+            if val >= 0:
+                item4.setForeground(QtGui.QBrush(QtGui.QColor('#191')))
+            else:
+                item4.setForeground(QtGui.QBrush(QtGui.QColor('#911')))
+            item5 = QtGui.QStandardItem('№' + lst4[row])
+            sti.appendRow([item1, item2, item3, item4, item5])
+            # sti.setProperty()
+
+        sti.setHorizontalHeaderLabels(['Назва', 'Наявна\nкількість', 'Необхідна\nкількість', 'Залишок', 'Договір'])
+        sti.setRowCount(15)
+        table.setModel(sti)
+        table.setColumnWidth(0, 200)
+        table.setColumnWidth(2, 100)
+        table.setColumnWidth(3, 100)
+        table.setColumnWidth(4, 100)
+        table.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectionBehavior.SelectRows)
+        table.setAlternatingRowColors(True)
+        table.setSortingEnabled(True)
+        table.resize(700, 500)
 
 
-    def initVBox(self):
+        mainArea.addWidget(table)
+        self.innerbox.addLayout(mainArea,  QtCore.Qt.AlignmentFlag.AlignCenter)
         pass
-        #vbox = QtWidgets.QVBoxLayout()
-        #vbox.addWidget(table)
-        #vbox.addWidget(lblLog)
-        #vbox.addWidget(logArea, QtCore.Qt.AlignmentFlag.AlignBottom)
-        #vbox.setStretch(0, 15)
-        #vbox.setStretch(1, 1)
-        #vbox.setStretch(2, 3)
+
+    def __initLayout2(self):
+        pass
 
     def initVMenu(self):
         #buttons
         button1 = QtWidgets.QPushButton("Створити договір")
-        #button1.setStyleSheet("background-color: {}; ".format(self.btnClr.name()))
         button2 = QtWidgets.QPushButton("Редагувати договір")
         button3 = QtWidgets.QPushButton("3")
         button3.setDisabled(True)
@@ -72,7 +109,6 @@ class Project(QtWidgets.QWidget):
         self.vMenu.addWidget(button5)
         self.vMenu.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
         self.vMenu.addStretch(40)
-        #self.setLayout(self.vMenu)
 
     def initColors(self):
         #using colors: #0d1321, #1d2d44, #3e5c76, #748cab, #f0ebd8
@@ -94,31 +130,8 @@ def main():
     app = QtWidgets.QApplication(sys.argv)
     ico = QtGui.QIcon("img/logo.png")
     app.setWindowIcon(ico)
-    app.setStyleSheet("""
-        QWidget {
-            background-color: "#1d2d44";
-            color: "#f0ebd8";
-        }
-        QPushButton {
-            font-size: 16px;
-            color: "#f0ebd8";
-            background-color: "#3e5c76"
-        }
-        QPushButton:pressed {
-            font-size: 16px;
-            color: "#f0ebd8";
-            background-color: "#1d2d44"
-        }
-        QPushButton:disabled {
-            font-size: 16px;
-            color: "#f0ebd8";
-            background-color: "#1d2d44"
-        }
-        QTextEdit, QLineEdit {
-            background-color: "#748cab";       
-            color: "#1d2d44"     
-        }
-    """)
+    with open("style.css", "r") as file:
+        app.setStyleSheet(file.read())
     pr = Project()
     sys.exit(app.exec())
 
